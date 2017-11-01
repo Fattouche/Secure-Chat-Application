9dd6709d24b5794f1cc79c3e8649919827db17da
Zenara Daley <zrdaley@gmail.com>
2017-11-01 16:43:10 -0700

    created a security class to remove code clones

--- Server.java
+++ Server.java
@@ -60,29 +60,7 @@ public class Server {
             if (args.length != 1) {
                   System.out.println("Usage: java server <port>");
             } else {
-                  // Prompt user to enable/disable security properties
-                  Scanner reader = new Scanner(System.in);
-                  String confidentiality, integrity, authenticaion;
-                  
-                  System.out.println("Require confidentiality? (y or n)");
-                  while(true){
-                        confidentiality = reader.nextLine();
-                        if(confidentiality.equals("y") || confidentiality.equals("n")) break; 
-                        System.out.println("Invalid input, try again.");
-                  }
-                  System.out.println("Require integrity? (y or n)");
-                  while(true){
-                        integrity = reader.nextLine();
-                        if(integrity.equals("y") || integrity.equals("n")) break; 
-                        System.out.println("Invalid input, try again.");
-                  }
-                  System.out.println("Require authenticaion? (y or n)");
-                   while(true){
-                        authenticaion = reader.nextLine();
-                        if(authenticaion.equals("y") || authenticaion.equals("n")) break; 
-                        System.out.println("Invalid input, try again.");
-                  }
-                  reader.close();
+                  Security security = new Security();
 
                   int portNumber = Integer.parseInt(args[0]);
                   try {
