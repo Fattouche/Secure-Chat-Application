3992eda5f6fd4e491165e24798e73a9582a7be82
Gurjyot <gurjyot.g@gmail.com>
2017-11-03 15:41:37 -0700

    Adding parser

---
+++ Communication.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.math.BigInteger;
+import java.nio.file.*;
+import java.lang.*;
+
+
+
+class Communication{
+		
+		public static byte[] message;
+		public static byte[] signature;
+		public static byte[] mac;
+
+		public Communication(byte[] com){
+			try{
+				parse(com);
+			}catch (Exception e) {
+				System.out.println(e);
+			}
+		}
+
+		public void parse(byte[] com) throws Exception{
+			String str = new String(com);
+			String[] portions = str.split(";;;");
+
+
+			if(portions.length > 3){
+				System.out.println("The Communication object contains incorrect coding");
+				
+			}
+
+			if(portions[0].equals("")){
+				System.out.println("The Communication object does not contain the message");
+			}else{
+				this.message = portions[0].getBytes("UTF8");
+				this.signature = portions[1].getBytes("UTF8");
+				this.mac = portions[2].getBytes("UTF8");
+			}
+		}
+}
