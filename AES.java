6839fad2b7e431952e8d615234348dee18c85761
Zenara Daley <zrdaley@gmail.com>
2017-11-03 06:54:40 -0700

    Added RSA siging

--- AES.java
+++ AES.java
@@ -2,14 +2,29 @@ import javax.crypto.Cipher;
 import javax.crypto.Mac;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
-import java.security.MessageDigest;
-import java.security.SecureRandom;
+// import java.security.spec.X509EncodedKeySpec;
+// import java.security.spec.PKCS8EncodedKeySpec
+import java.security.spec.*;
+import java.security.*;
 import java.io.*;
 import java.util.*;
+import java.nio.file.*;
 
 public class AES {
 	static private int ivSize = 16;
 	static private int keySize = 16;
+
+	private static PublicKey readPublicKey(Path path) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
+	    X509EncodedKeySpec publicSpec = new X509EncodedKeySpec(Files.readAllBytes(path));
+	    KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+	    return keyFactory.generatePublic(publicSpec);       
+	}
+
+	private static PrivateKey readPrivateKey(Path path) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
+	    PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(Files.readAllBytes(path));
+	    KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+	    return keyFactory.generatePrivate(keySpec);     
+	}
 
 	public static Boolean compareMAC(byte[] m1, byte[] m2) throws IOException {
 		if (!Arrays.equals(m1, m2))
@@ -33,10 +48,10 @@ public class AES {
 		}
 	}
 
-	public static byte[] digestMessage(String message) {
+	public static byte[] digestMessage(byte[] message) {
 		try{
-			MessageDigest digest = MessageDigest.getInstance("SHA-256");
-			digest.update(message.getBytes());
+			MessageDigest digest = MessageDigest.getInstance("SHA1");
+			digest.update(message);
 			byte[] messageDigest = digest.digest();
 			return messageDigest;
 		}
@@ -51,7 +66,40 @@ public class AES {
 		return true;
 	}
 
-	public static byte[] encrypt(String message, String key) {
+	public static byte[] createSignature(byte[] digest, Path privateKeyPath) {
+		try{
+			PrivateKey privateKey = readPrivateKey(privateKeyPath);
+			Cipher cipher = Cipher.getInstance("RSA");
+			cipher.init(Cipher.ENCRYPT_MODE, privateKey);
+			byte[] signedDigest = cipher.doFinal(digest);
+			return signedDigest;
+		}
+		catch (Exception e) {
+			System.out.println("Error in AES.createSignature: " + e);
+			return null;
+		}
+	}
+
+	public static Boolean checkSignature(byte[] signature, Path publicKeyPath, byte[] message) {
+		try{
+			PublicKey publicKey = readPublicKey(publicKeyPath);
+
+			Cipher cipher = Cipher.getInstance("RSA");
+			cipher.init(Cipher.DECRYPT_MODE, publicKey);
+			byte[] digestFromSignature = cipher.doFinal(signature);
+			
+			byte[] digestFromMessage = digestMessage(message);
+			
+			if (Arrays.equals(digestFromMessage, digestFromSignature)) return true;
+			return false;
+		}
+		catch (Exception e) {
+			System.out.println("Error in AES.checkSignature: " + e);
+			return null;
+		}
+	}
+
+	public static byte[] encrypt(byte[] message, byte[] key) {
 		try {
 			// Generating IV Spec
 			byte[] iv = new byte[ivSize];
@@ -60,7 +108,7 @@ public class AES {
 			IvParameterSpec ivSpec = new IvParameterSpec(iv);
 
 			// Generate keySpec
-			SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(), "AES");
+			SecretKeySpec keySpec = new SecretKeySpec(key, "AES");
 
 			// Create and initialize the cipher for encryption
 			Cipher aesCipher;
@@ -68,7 +116,7 @@ public class AES {
 			aesCipher.init(Cipher.ENCRYPT_MODE, keySpec, ivSpec);
 
 			// Encrypt the cleartext
-			byte[] cleartext = message.getBytes();
+			byte[] cleartext = message;
 			byte[] ciphertext = aesCipher.doFinal(cleartext);
 
 			//return IV + ciphertext
@@ -82,7 +130,7 @@ public class AES {
 		}
 	}
 
-	public static String decrypt(byte[] encryptedMessage, String key) {
+	public static byte[] decrypt(byte[] encryptedMessage, byte[] key) {
 		try {
 			// Split the encrypted message into IV and Ciphertext
 			byte[] iv = new byte[ivSize];
@@ -92,7 +140,7 @@ public class AES {
 
 			// Create ivSpec and keySpec
 			IvParameterSpec ivSpec = new IvParameterSpec(iv);
-			SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(), "AES");
+			SecretKeySpec keySpec = new SecretKeySpec(key, "AES");
 
 			// Create and initialize the cipher for encryption
 			Cipher aesCipher;
@@ -101,32 +149,33 @@ public class AES {
 
 			// Encrypt the cleartext
 			byte[] cleartext = aesCipher.doFinal(ciphertext);
-			return new String(cleartext);
+			return cleartext;
 		} catch (Exception e) {
 			System.out.println("Error in AES.decrypt: " + e);
 			return null;
 		}
 	}
 	
-	/*
+	
 	// THIS IS USED FOR TESTING PURPOSED ONLY
 
 	public static void main(String args[]) {
-		String message = "HELLO WORLD";
-		String key = "0123456789abcdef";
+		
 	
 		try {
-			byte[] d1 = digestMessage(message);
-			byte[] d2 = digestMessage(message);
+			byte[] message = "HELLO WORLD".getBytes("UTF8");
+			byte[] digest = digestMessage(message);
+
+			Path pvk = Paths.get("server_private", "private.der");
+			Path pubk = Paths.get("client_private", "publicServer.der");
+
+			byte[] a = createSignature(digest, pvk);
 	
-			byte[] e = encrypt(message, key);
-			String d = decrypt(e, key);
+			if(!checkSignature(digest, pubk, message)) System.out.println("No Bueno");
 	
-			if(!compareDigests(d1, d2)) System.out.println("No Bueno");
-	
-			if (!message.equals(d)) System.out.println("No bueno");
+			// if (!message.equals(d)) System.out.println("No bueno");
 		
 		} catch(Exception e) {}
 	}
-	*/
+	
 }
